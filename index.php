cb1edc83fce656464adcf3dd418de3074733ab27
chumengyong <2415714970@qq.com>
2022-06-29 12:04:40 +0800

    可以在微软客户端上挂载

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ class WebDav
     {
         $this->prefix = $prefix;
         $this->scope = $scope;
-        $this->uri = get_current_uri($_SERVER['REQUEST_URI']);
+        $this->uri = get_current_uri(urldecode($_SERVER['REQUEST_URI']));
         $this->path = $this->scope . DIRECTORY_SEPARATOR . $this->uri;
     }
 
@@ -25,66 +25,26 @@ class WebDav
      * @return void
      * @throws DOMException
      */
-    public function propfind()
+    public function propfind(): void
     {
         $dom = new DOMDocument('1.0', 'utf-8');
         $start_node = $dom->createElement('D:multistatus');
         $start_node->setAttribute('xmlns:D', 'DAV:');
         $dom->appendChild($start_node);
 
-        $files = get_files($this->path);
+        if (isset($_SERVER['HTTP_DEPTH']) && $_SERVER['HTTP_DEPTH'] == 0) {
+            $f = get_files($this->path, false);
+            set_response_node($f, $dom, $start_node, $this->uri);
+        } else {
+            $self_path_file = get_files($this->path, false);
+            set_response_node($self_path_file, $dom, $start_node, $this->uri, true);
 
-        // 生成 D:response
-        foreach ($files as $file) {
-            $response_node = $dom->createElement('D:response');
-            // 给 response 加 href 属性
-            $href_node = $dom->createElement('D:href');
-            // 遍历出来的文件名
-            $href_node->textContent = '/' . $file['filename'];
-            $response_node->appendChild($href_node);
-
-            // 文件属性
-            $prostate_node = $dom->createElement('D:prostate');
-            $status_node = $dom->createElement('D:status');
-            $status_node->textContent = 'HTTP/1.1 200 OK';
-
-            $prop_node = $dom->createElement('D:prop');
-            $getLastModified_node = $dom->createElement('D:getlastmodified');
-
-            $getLastModified_node->textContent = $file['lastModified'];
-            $prop_node->appendChild($getLastModified_node);
-
-            // 判断是否是文件夹, 如果是文件夹的话, 就不用显示大小了
-            if (!$file['is_dir']) {
-                $content_length_node = $dom->createElement('D:getcontentlength');
-                $content_length_node->textContent = $file['contentLength'];
-                $prop_node->appendChild($content_length_node);
+            foreach (get_files($this->path) as $file) {
+                set_response_node($file, $dom, $start_node, $this->uri);
             }
-
-            // 显示文件名
-            $displayname_node = $dom->createElement('D:displayname');
-            $displayname_node->textContent = $file['filename'];
-
-            // 判断是否为文件夹
-            $resource_type_node = $dom->createElement('D:resourcetype');
-            // 如果是文件夹, 那我们得再加一层
-            if ($file['is_dir']) {
-                $collection_node = $dom->createElement('D:collection');
-                $collection_node->setAttribute('xmlns:D', 'DAV:');
-                $resource_type_node->appendChild($collection_node);
-            }
-            $prop_node->appendChild($resource_type_node);
-
-
-            $prop_node->appendChild($displayname_node);
-
-            $prostate_node->appendChild($prop_node);
-            $prostate_node->appendChild($status_node);
-            $response_node->appendChild($prostate_node);
-
-            $start_node->appendChild($response_node);
         }
 
+        header('HTTP/1.1 207 Multi-Status');
         header('Content-Type: text/xml');
         echo $dom->saveXML();
     }
@@ -95,12 +55,41 @@ class WebDav
     public function get()
     {
         if (is_file($this->path)) {
-            $fh = fopen($this->path, 'r');
-            $oh = fopen('php://output', 'w');
-            stream_copy_to_stream($fh, $oh);
-            fclose($fh);
-            fclose($oh);
+            $fileObj = new SplFileObject($this->path, 'r');
+            // 如果用户执行了请求范围, 则分次读取, 否则全部返回
+            if (isset($_SERVER['HTTP_RANGE'])) {
+                $exploded = explode('-', $_SERVER['HTTP_RANGE'], 2);
+                $start = substr($exploded[0], 6);
+                $end = $exploded[1];
+                $start = intval($start);
+                $end = intval($end);
+                $r = $end - $start;
+                header('HTTP/1.1 206 Partial Content');
+                header('Accept-Ranges: bytes');
+                header('Content-Length: ' . $r);
+                header("Content-Range: bytes $start-$end");
+                header('Last-Modified: ' . get_gmt_time($fileObj->getMTime()));
+                header('Etag: ' . get_etag_by_path($this->path));
+                set_content_type_header_by_path($this->path);
+                // 如果分片过大
+                $fileObj->fseek($start);
+                echo $fileObj->fread($r);
+            } else {
+                set_content_type_header_by_path($this->path);
+                while (!$fileObj->eof()) {
+                    echo $fileObj->fgets();
+                }
+            }
         }
+    }
+
+    /** 当客户端访问我们的服务器时
+     *  我们得返回所有支持的方法
+     * @return void
+     */
+    public function options(): void
+    {
+        header('Allow: OPTIONS, GET, PUT, PROPFIND, PROPPATCH');
     }
 
 }
--- utils.php
+++ utils.php
@@ -1,32 +1,47 @@
 <?php
 
+require __DIR__ . '/ContentType.php';
+
 /**
  * @throws Exception
  */
-function get_files(string $dirs): array
+function get_files(string $dirs, bool $flag = true): array
 {
-    $file_array = [];
-    try {
-        $directoryIterator = new DirectoryIterator($dirs);
-    } catch (Exception $e) {
-        die('404 Not Found');
-    }
-
-    foreach ($directoryIterator as $fileObj) {
-        if ($fileObj->getFilename() === '.' || $fileObj->getFilename() === '..') {
-            continue;
+    if ($flag) {
+        $file_array = [];
+        try {
+            $directoryIterator = new DirectoryIterator($dirs);
+        } catch (Exception $e) {
+            die('404 Not Found');
         }
 
+        foreach ($directoryIterator as $fileObj) {
+            if ($fileObj->getFilename() === '.' || $fileObj->getFilename() === '..') {
+                continue;
+            }
+
+            $gmt_time = date('D, d M Y H:i:s', $fileObj->getMTime()) . ' GMT';
+            $tmp_array = [];
+            $tmp_array['lastModified'] = $gmt_time;
+            $tmp_array['contentLength'] = $fileObj->getSize();
+            $tmp_array['is_dir'] = $fileObj->isDir();
+            $tmp_array['filename'] = $fileObj->getFilename();
+            $tmp_array['file_path'] = $fileObj->getPath();
+            $file_array[] = $tmp_array;
+        }
+
+        return $file_array;
+    } else {
+        $fileObj = new SplFileInfo($dirs);
         $gmt_time = date('D, d M Y H:i:s', $fileObj->getMTime()) . ' GMT';
         $tmp_array = [];
         $tmp_array['lastModified'] = $gmt_time;
         $tmp_array['contentLength'] = $fileObj->getSize();
         $tmp_array['is_dir'] = $fileObj->isDir();
         $tmp_array['filename'] = $fileObj->getFilename();
-        $file_array[] = $tmp_array;
+        $tmp_array['file_path'] = $fileObj->getPath();
+        return $tmp_array;
     }
-
-    return $file_array;
 
 }
 
@@ -38,4 +53,128 @@ function get_current_uri(string $uri): string
     }
 
     return $uri;
+}
+
+function get_gmt_time(string $timestamp = null): string
+{
+    if (isset($timestamp)) {
+        return date('D, d M Y H:i:s', intval($timestamp)) . ' GMT';
+    }
+
+    return date('D, d M Y H:i:s') . ' GMT';
+}
+
+/** 传入路径， 根据路径获取文件后缀名
+ *  根据后缀名设置 Content_type
+ * @param string $path
+ * @return void
+ */
+function set_content_type_header_by_path(string $path): void
+{
+    global $content_type_array;
+    $suffix = substr($path, strrpos($path, '.') + 1);
+    $content_type = $content_type_array[$suffix] ?? 'application/octet-stream';
+    header('Content-type: ' . $content_type);
+}
+
+function get_content_type_by_path(string $path): string
+{
+    global $content_type_array;
+    $suffix = substr($path, strrpos($path, '.') + 1);
+    return $content_type_array[$suffix] ?? 'application/octet-stream';
+}
+
+/** 通过对文件名和文件修改时间进行 sha1 计算得出 etag
+ * @param string $path
+ * @return string
+ */
+function get_etag_by_path(string $path): string
+{
+    $fileInfoObj = new SplFileInfo($path);
+    return '"' . sha1(strval($fileInfoObj->getMTime()) . $fileInfoObj->getFilename()) . '"';
+}
+
+
+/** 根据用户传递过来的文件数组, 生成 response DOM 树
+ * [
+ *      'lastModified' => 'xxx',
+ *      'contentLength' => 'xxx',
+ *      'is_dir' => true|false,
+ *      'filename' => xxx,
+ *      'file_path' => 'xxxx'
+ * ]
+ * @param array $files
+ * @param DOMDocument $dom
+ * @param DOMElement $start_node
+ * @param string $uri
+ * @return void
+ * @throws DOMException
+ */
+function set_response_node(array $files, DOMDocument &$dom, DOMElement &$start_node, string $uri, bool $is_first_node = false): void
+{
+    $response_node = $dom->createElement('D:response');
+    $href_node = $dom->createElement('D:href');
+    if ($is_first_node) {
+        if ($uri[-1] === '/') {
+            $href_node->textContent = substr($uri, 0, strlen($uri) - 1);
+        } else {
+            $href_node->textContent = $uri;
+        }
+    } elseif ($uri === '/') {
+        $href_node->textContent = $uri . $files['filename'];
+    } elseif ($uri[-1] == '/') {
+        $href_node->textContent = $uri . $files['filename'];
+    }
+    else {
+        $href_node->textContent = $uri . '/' . $files['filename'];
+    }
+
+    $response_node->appendChild($href_node);
+
+    $prostate_node = $dom->createElement('D:prostate');
+    $status_node = $dom->createElement('D:status');
+    $status_node->textContent = 'HTTP/1.1 200 OK';
+
+    $prop_node = $dom->createElement('D:prop');
+    $getLastModified_node = $dom->createElement('D:getlastmodified');
+    $getLastModified_node->textContent = $files['lastModified'];
+    $prop_node->appendChild($getLastModified_node);
+
+    if (!$files['is_dir']) {
+        $content_length_node = $dom->createElement('D:getcontentlength');
+        $content_length_node->textContent = $files['contentLength'];
+        $prop_node->appendChild($content_length_node);
+    }
+
+    $displayname_node = $dom->createElement('D:displayname');
+    $displayname_node->textContent = $files['filename'];
+
+    $resource_type_node = $dom->createElement('D:resourcetype');
+
+    if ($files['is_dir']) {
+        $collection_node = $dom->createElement('D:collection');
+        $collection_node->setAttribute('xmlns:D', 'DAV:');
+        $resource_type_node->appendChild($collection_node);
+    }
+    $prop_node->appendChild($resource_type_node);
+
+    if (!$files['is_dir']) {
+        $content_type_node = $dom->createElement('D:getcontenttype');
+        $content_type_node->textContent = get_content_type_by_path($files['file_path'] . DIRECTORY_SEPARATOR . $files['filename']);
+        $prop_node->appendChild($content_type_node);
+    }
+
+    if (!$files['is_dir']) {
+        $etag_node = $dom->createElement('D:getetag');
+        $etag_node->textContent = get_etag_by_path($files['file_path'] . DIRECTORY_SEPARATOR . $files['filename']);
+        $prop_node->appendChild($etag_node);
+    }
+
+    $prop_node->append($displayname_node);
+
+    $prostate_node->appendChild($prop_node);
+    $prostate_node->appendChild($status_node);
+    $response_node->appendChild($prostate_node);
+
+    $start_node->appendChild($response_node);
 }
